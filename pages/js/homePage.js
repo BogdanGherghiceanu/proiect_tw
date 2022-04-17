942eebadac7aaf3fad48910ef53cf37ecfadd9a4
Tiganasu Cristina-Ecaterina <79219863+TiganasuCristinaEcaterina@users.noreply.github.com>
2022-04-17 22:49:33 +0300

    Small updates

---
+++ pages/js/homePage.js
@@ -0,0 +1,24 @@
+function openLoginModal() {
+
+    var modal = document.getElementById("loginModal");
+
+    $('#mydropdown').toggleClass('show');
+    modal.style.display = "block";
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+
+        if (!event.target.matches('.navbar-icon')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+}
