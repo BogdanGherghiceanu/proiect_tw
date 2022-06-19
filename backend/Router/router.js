54ce298210e21a9768061e6bf31d0f528118c372
Tiganasu Cristina-Ecaterina <79219863+TiganasuCristinaEcaterina@users.noreply.github.com>
2022-06-19 14:13:42 +0300

    Update router.js

--- backend/Router/router.js
+++ backend/Router/router.js
@@ -1,6 +1,6 @@
 const homeController = require("../Controller/homeController");
 const userProfileController = require("../Controller/userProfileController");
-const login = require("../Controller/loginController");
+const adminProfileController = require("../Controller/adminProfileController");
 
 const {getFile} = require("../Utils/utils");
 
@@ -21,10 +21,10 @@ route_post = (url, action, middleware) => {
 route_get("/", homeController);
 route_get("/pages/index.html", homeController);
 route_get("/pages/userProfile.html", userProfileController);
+route_get("/pages/adminProfile.html", adminProfileController);
 
 //POST routes
-route_post("/api/login", login.loginController);
-route_post("/pages/userProfile.html", userProfileController);
+// route_post("/pages/userProfile.html", userProfileController);
 
 
 //STATIC FILES
@@ -32,11 +32,13 @@ route_post("/pages/userProfile.html", userProfileController);
 route_get("/pages/style/nav.css", (req, res) => getFile("../pages/style/nav.css", res));
 route_get("/pages/style/homepage.css", (req, res) => getFile("../pages/style/homepage.css", res));
 route_get("/pages/style/userProfile.css", (req, res) => getFile("../pages/style/userProfile.css", res));
+route_get("/pages/style/adminProfile.css", (req, res) => getFile("../pages/style/adminProfile.css", res));
 
 //JS
 route_get("/pages/js/homePage.js", (req, res) => getFile("../pages/js/homePage.js", res));
 route_get("/pages/js/login.js", (req, res) => getFile("../pages/js/login.js", res));
 route_get("/pages/js/userProfile.js", (req, res) => getFile("../pages/js/userProfile.js", res));
+route_get("/pages/js/adminProfile.js", (req, res) => getFile("../pages/js/adminProfile.js", res));
 
 //IMAGES
 route_get("/pages/resources/index/1.png", (req, res) => getFile("../pages/resources/index/1.png", res));
@@ -45,6 +47,10 @@ route_get("/pages/resources/index/3.png", (req, res) => getFile("../pages/resour
 route_get("/pages/resources/userProfile/bike_icon.png", (req, res) => getFile("../pages/resources/userProfile/bike_icon.png", res));
 route_get("/pages/resources/userProfile/calendar_icon.png", (req, res) => getFile("../pages/resources/userProfile/calendar_icon.png", res));
 route_get("/pages/resources/navbar/tyre.png", (req, res) => getFile("../pages/resources/navbar/tyre.png", res));
+route_get("/pages/resources/adminProfile/bike_icon.png", (req, res) => getFile("../pages/resources/adminProfile/bike_icon.png", res));
+route_get("/pages/resources/adminProfile/calendar_icon.png", (req, res) => getFile("../pages/resources/adminProfile/calendar_icon.png", res));
+route_get("/pages/resources/adminProfile/warehouse_icon.png", (req, res) => getFile("../pages/resources/adminProfile/warehouse_icon.png", res));
+route_get("/pages/resources/adminProfile/supplier_icon.png", (req, res) => getFile("../pages/resources/adminProfile/supplier_icon.png", res));
 
 const url_param_check = (url) => {
     let id = "";
